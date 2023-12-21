3d8efa5ed9eae9284585a3114fd379824c34abd2
po' dev <legopowa@hotmail.com>
2023-12-21 11:19:54 -0800

    cobbling together tfc contracts

--- GPTFC_alpha.sol
+++ GPTFC_alpha.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.0;
 contract TFCPlayAndEarn {
     address public owner;
     mapping(address => bool) public isValidator;
-    // Other mappings and variables...
+    mapping(address => string[]) public validatorSubmissions;
+    mapping(string => bool) private nameExists; // To check if a name has already been added
+    string[] private allSubmittedNames; // To store all unique names
+
+    uint256 public lastMintingTime;
+    uint256 public interval = 300; // Interval in seconds
 
     constructor() {
         owner = msg.sender;
@@ -18,7 +23,21 @@ contract TFCPlayAndEarn {
         require(msg.sender == owner, "Only the owner can remove validators");
         isValidator[validator] = false;
     }
+    address public parentContract;
 
+    // Address of the AnonID contract
+    address public anonIDContractAddress;
+
+    // Set AnonID Contract Address
+    function setAnonIDContractAddress(address _address) public {
+        require(msg.sender == owner, "Only the owner can set the AnonID contract address");
+        anonIDContractAddress = _address;
+    }
+
+    function setParentContract(address _parentContract) public {
+        require(msg.sender == owner, "Only the owner can set the parent contract");
+        parentContract = _parentContract;
+    }
     // Other contract members...
 
     mapping(address => string[]) public validatorSubmissions;
@@ -26,11 +45,16 @@ contract TFCPlayAndEarn {
     uint256 public interval = 300; // Example interval in seconds
 
     function submitNames(string[] memory names) public {
-        // Only validators can submit names
-        require(isValidator(msg.sender), "Not a validator");
+        require(isValidator[msg.sender], "Not a validator");
         validatorSubmissions[msg.sender] = names;
-        
-        // Check if we are past the interval and trigger minting
+
+        for(uint i = 0; i < names.length; i++) {
+            if (!nameExists[names[i]]) {
+                nameExists[names[i]] = true;
+                allSubmittedNames.push(names[i]);
+            }
+        }
+
         if (block.timestamp >= lastMintingTime + interval) {
             mintTokens();
             lastMintingTime = block.timestamp;
@@ -38,10 +62,23 @@ contract TFCPlayAndEarn {
     }
 
     function mintTokens() private {
-        // Process to compare names from different validators
-        // and find common names which appear in at least 1/4 of submissions
-        // Implement the logic to find common names and mint tokens
+        ERC20Token parent = ERC20Token(parentContract);
+        AnonIDContract anonID = AnonIDContract(anonIDContractAddress);
+        uint256 currentMintTime = block.timestamp;
+        uint256 mintAmount;
+
+        for (uint256 i = 0; i < playerAddresses.length; i++) {
+            address playerAddress = playerAddresses[i];
+            
+            // Check if the player is active in the game
+            if (anonID.isPlayerActiveInGame(currentGameID, playerAddress)) {
+                // Calculate the mint amount based on active time
+                mintAmount = calculateMintAmount(anonID.getLastPlayedTimestamp(playerAddress), currentMintTime);
+                parent.mint(playerAddress, mintAmount); // Minting from the parent contract
+            }
+        }
     }
+
     function registerPlayer(string memory tfcName, address playerAddress) public {
         require(msg.sender == owner, "Only the owner can register players");
         tfcNameToAddress[tfcName] = playerAddress;
